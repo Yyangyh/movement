2bd606e1fa1fe2b687185d5ea535fa55696cc521
yyh <286441209@qq.com>
2019-06-04 10:52:35 +0800

    2019-6-4-10.52 Signed-off-by: yyh <286441209@qq.com>

--- Json.js
+++ Json.js
@@ -4,6 +4,7 @@ const userInfo = {
 	data: {
 		id: 1,
 		mobile: 18888888888,
+		mobile: 18888888888,
 		nickname: 'Leo yo',
 		portrait: 'http://img.61ef.cn/news/201409/28/2014092805595807.jpg'
 	},
